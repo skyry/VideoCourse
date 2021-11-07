e00d3f27947b9649fb1911c32d94c972aa631d40
skyry <33283945+skyry@users.noreply.github.com>
2021-11-07 23:27:18 +0200

    Initial commit

---
+++ src/HomeWork/Lesons2.java
@@ -0,0 +1,73 @@
+package HomeWork;
+
+public class Lesons2 {
+    public static void main(String[] args) {
+        byte b1 = 12;
+        byte b2 = 0b1100;
+        byte b3 = 0xc;
+        byte b4 = 014;
+
+        System.out.println(b1);
+        System.out.println(b2);
+        System.out.println(b3);
+        System.out.println(b4);
+
+        short s1 = 1300;
+        short s2 = 0b0101_0001_0100;
+        short s3 = 0x514;
+        short s4 = 02424;
+
+        System.out.println(s1);
+        System.out.println(s2);
+        System.out.println(s3);
+        System.out.println(s4);
+
+        int i1 = 0;
+        int i2 = 0b0;
+        int i3 = 0x0;
+        int i4 = 00;
+
+        System.out.println(i1);
+        System.out.println(i2);
+        System.out.println(i3);
+        System.out.println(i4);
+
+        long l1 = 123456789;
+        long l2 = 0b0111_0101_1011_1100_1101_0001_0101;
+        long l3 = 0x75BCD15;
+        long l4 = 0726746425;
+
+        System.out.println(l1);
+        System.out.println(l2);
+        System.out.println(l3);
+        System.out.println(l4);
+
+        float f1 = 12.5f;
+        float f2 = 48.9f;
+
+        System.out.println(f1);
+        System.out.println(f2);
+
+        double d1 = 0.8;
+        double d2 = 89.4;
+
+        System.out.println(d1);
+        System.out.println(d2);
+
+        boolean bol1 = true;
+        boolean bol2 = false;
+
+        System.out.println(bol1);
+        System.out.println(bol2);
+
+        char c1 = 'f';
+        char c2 = 345;
+        char c3 = '\u0345';
+
+        System.out.println(c1);
+        System.out.println(c2);
+        System.out.println(c3);
+
+
+    }
+}
---
+++ src/Lesson4/BankAccount.java
@@ -0,0 +1,12 @@
+package Lesson4;
+
+public class BankAccount {
+    int id = 1;
+    String name ;
+    double balance;
+
+
+    public static void main(String[] args) {
+        BankAccount bA = new BankAccount();
+    }
+}
