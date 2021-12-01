cbfc7dbdb2ac9f5126b274c157bd94b73aa527fb
skyry <33283945+skyry@users.noreply.github.com>
2021-12-01 23:13:13 +0200

    L16

---
+++ src/Lesson16/Test9.java
@@ -0,0 +1,10 @@
+package Lesson16;
+
+public class Test9 {
+    public static void main(String[] args) {
+
+        String s1 = new String("OK");
+        String s2 = new String("OK");
+        System.out.println(s1.equals(s2));
+    }
+}
