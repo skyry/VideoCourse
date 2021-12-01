14dd792c4b0a2d4f2213454f8b4d630922505459
skyry <33283945+skyry@users.noreply.github.com>
2021-12-01 23:40:52 +0200

    L16

---
+++ src/HomeWork/Lessons16/Mail.java
@@ -0,0 +1,15 @@
+package HomeWork.Lessons16;
+
+public class Mail {
+
+    public void email(String text){
+
+        char c1;
+        char c2;
+
+        for (int i = 0; i<text.length(); i++){
+
+        }
+
+    }
+}
