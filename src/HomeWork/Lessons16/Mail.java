8db36f0d1530dfa09afb6d5249993c26df65b779
Tems_PC <lukey868@gmail.com>
2021-12-02 16:30:30 +0200

    Lesons16

--- src/HomeWork/Lessons16/Mail.java
+++ src/HomeWork/Lessons16/Mail.java
@@ -1,15 +1,35 @@
 package HomeWork.Lessons16;
 
+
 public class Mail {
 
     public void email(String text){
+        String t = text;
+        int c1 = 0;
+        int c2 = 0;
+        char c3 = ' ';
 
-        char c1;
-        char c2;
+        for (int i = 0; i<t.length(); i++){
+            if (t.charAt(i) == '@'){
+                t.charAt(i)!= ';'
+            }){
+                System.out.print(t.charAt(i+1));
 
-        for (int i = 0; i<text.length(); i++){
+            }
+            else {
+                System.out.println();
+              }
+
+            }
 
         }
 
+
+    public static void main(String[] args) {
+        Mail mail1 = new Mail();
+        mail1.email("ya@yahoo.com; on@mail.ru; ona@gmail.com");
     }
+
+
+
 }
