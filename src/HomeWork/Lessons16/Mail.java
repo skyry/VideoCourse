720ca68bd55a4aa11513adaba1e93e09be944339
skyry <33283945+skyry@users.noreply.github.com>
2021-12-02 23:21:15 +0200

    L16

--- src/HomeWork/Lessons16/Mail.java
+++ src/HomeWork/Lessons16/Mail.java
@@ -3,35 +3,28 @@ package HomeWork.Lessons16;
 
 public class Mail {
 
-    public void email(String text){
+    public void email(String text) {
         String t = text;
         int c1 = 0;
         int c2 = 0;
-        char c3 = ' ';
 
-        for (int i = 0; i<t.length(); i++){
-            if (t.charAt(i) == '@'){
-                System.out.print(t.charAt(i+1));
-                    }
-            if (t.charAt(i) != '.'){
-                System.out.print(t.charAt(i));
+        for (int i = 0; i < t.length(); i++) {
+
+            if (t.charAt(i) == '@') {
+                c1 = (t.indexOf('@', i)) + 1;
             }
-
-            else {
-                System.out.println();
+            if (t.charAt(i) == '.') {
+                c2 = t.indexOf('.', i);
+                System.out.println(t.substring(c1, c2));
             }
-
-
-            }
-
         }
+    }
 
 
     public static void main(String[] args) {
         Mail mail1 = new Mail();
         mail1.email("ya@yahoo.com; on@mail.ru; ona@gmail.com");
     }
-
 
 
 }
