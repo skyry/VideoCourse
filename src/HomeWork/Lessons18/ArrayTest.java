edfb629c38f20eb937d977cce0dd1dd24d5baac8
skyry <33283945+skyry@users.noreply.github.com>
2022-01-05 22:13:44 +0200

    L18

--- src/HomeWork/Lessons18/ArrayTest.java
+++ src/HomeWork/Lessons18/ArrayTest.java
@@ -4,27 +4,27 @@ import java.util.Arrays;
 
 public class ArrayTest {
 
-    public static int[] sortirovka(int[] array) {
+    public static void sortirovka(int[] array) {
         //Arrays.sort(array);
         int a;
+        int b;
         for (int i = 0; i < array.length; i++) {
             int index1 = array[i];
             for (int j=i+1; j<array.length;j++){
                 int index2 = array[j];
-
+                if (index1>index2){
+                    a=index1;
+                }
             }
         }
-        return array;
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i]);
+        }
     }
 
     public static void main(String[] args) {
         int[] array = {9, 15, 5, 2, 4, 8, 1, 0};
         sortirovka(array);
-
-        for (int i = 0; i < array.length; i++) {
-            System.out.print(array[i]);
-        }
-
     }
 
 }
---
+++ src/Lesson18/Test9.java
@@ -0,0 +1,26 @@
+package Lesson18;
+
+public class Test9 {
+    public static void main(String[] args) {
+        //boolean a =false;
+    boolean b =false;
+    boolean c=true;
+
+        //System.out.println(!a && !b && !c);
+
+        //System.out.print(3 + "1" + "b" + (1 + 1) + (1 + 3) + 'h');
+
+        int []a = {3, 2, 1, 5, 6, 7};
+
+        int i = a.length - 1;
+
+        while (i > 0) {
+
+            --i;
+
+            System.out.print(a[i]);
+
+        }
+
+    }
+}
