cef88c8f9278b1df7f51237f68028fc32884be01
skyry <33283945+skyry@users.noreply.github.com>
2022-05-06 20:47:14 +0300

    23.01

--- src/HomeWork/Lessons18/ArrayTest2.java
+++ src/HomeWork/Lessons18/ArrayTest2.java
@@ -2,8 +2,16 @@ package HomeWork.Lessons18;
 
 public class ArrayTest2 {
     public static void showArray(String[][] array) {
-        System.out.print("{ {" + array[0][0] + ", " + array[0][1] + "}, {" + array[1][0] + "}, {" + array[1][0] + ", " + array[1][1] + "} }");
-    }
+        System.out.print("{ {");
+        for (int i=0; i<=array.length; i++){
+            for (int j = 0; j< array.length; j++){
+                //if (i=0 && j=0){
+
+                }
+            }
+        }
+
+        //System.out.print("{ {" + array[0][0] + ", " + array[0][1] + "}, {" + array[1][0] + "}, {" + array[1][0] + ", " + array[1][1] + "} }");
 
     public static void main(String[] args) {
         String[][] array = {{"Privet", "Poka"}, {"Hi", "Hiiii"}, {"Hello", "World"}};
---
+++ src/HomeWork/Lessons18/ForTest.java
@@ -0,0 +1,11 @@
+package HomeWork.Lessons18;
+
+public class ForTest {
+    public static void main(String[] args) {
+        int num=0;
+        for (int i=0; i<100; i++){
+            num = num++;
+            System.out.println(num);
+        }
+    }
+}
