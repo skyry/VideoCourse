557818c1ada1d63515f4a797912bf0b506eef53e
skyry <33283945+skyry@users.noreply.github.com>
2022-06-02 23:49:44 +0300

    02.06.22

---
+++ src/Lessons25/Test6.java
@@ -0,0 +1,18 @@
+package Lessons25;
+
+public class Test6 {
+    public static void main(String[] args) {
+        byte b = 10;
+        int a = b;
+        System.out.println(7);
+        int i1 = 3;
+        short s2 = (short) i1;
+        byte b2 = 127;
+        short s1 = 5000;
+        char c1 = 100;
+        long l1 = 1;
+        int i2 = i1;
+        int i10 = 111111;
+        short s10 = (short) i10;
+    }
+}
