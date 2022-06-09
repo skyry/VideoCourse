3492d171fec2cea9ffc7128ac565437eefceff93
Tems_PC <lukey868@gmail.com>
2022-06-09 15:22:04 +0300

    09.06.22

--- src/JR/Array/Test5.java
+++ src/JR/Array/Test5.java
@@ -1,4 +1,18 @@
 package JR.Array;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
 public class Test5 {
+    public static void main(String[] args) {
+        ArrayList<String> list = new ArrayList<>();
+        for (int i = 0; i < 5; i++) {
+            list.add("Hello - " + i);
+        }
+        System.out.println("Size ArrayList - " + list.size());
+        for (String i : list) {
+            System.out.println(i);
+
+        }
+    }
 }
