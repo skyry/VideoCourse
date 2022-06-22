0a5b5d0635324331f41a0869ffc16d3fbf293637
skyry <33283945+skyry@users.noreply.github.com>
2022-06-22 23:43:12 +0300

    22.06.22

---
+++ src/Lessons31/Test4.java
@@ -0,0 +1,9 @@
+package Lessons31;
+
+public class Test4 {
+    public static void main(String[] args) {
+        int[] array1= {-3,2,0,8,1};
+        int[] array2 = {9,0,4,-10};
+        System.out.println(array1[1]);
+    }
+}
